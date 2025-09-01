efebd7d70438ed23e643e2f2ed861dbb89b55068
Atruo <banyuls98@gmail.com>
2025-09-01 15:09:42 +0200

    fix: hUSD Id Contract

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,36 +1,41 @@
-import { HashpackConnector, KabilaConnector, HWCConnector } from "@buidlerlabs/hashgraph-react-wallets/connectors";
+import {
+    HashpackConnector,
+    KabilaConnector,
+    HWCConnector,
+} from '@buidlerlabs/hashgraph-react-wallets/connectors'
 
 export const SUPPORTED_WALLETS = [
     {
-      id: 'hashpack',
-      name: 'Hashpack',
-      icon: '/hashpack-wallet.png',
-      iconSize: { width: 32, height: 32 },
-      connector: HashpackConnector,
-      mobileSupported: false
+        id: 'hashpack',
+        name: 'Hashpack',
+        icon: '/hashpack-wallet.png',
+        iconSize: { width: 32, height: 32 },
+        connector: HashpackConnector,
+        mobileSupported: false,
     },
     {
-      id: 'kabila',
-      name: 'Kabila',
-      icon: '/kabila-wallet.png',
-      iconSize: { width: 22, height: 22 },
-      connector: KabilaConnector,
-      mobileSupported: false
+        id: 'kabila',
+        name: 'Kabila',
+        icon: '/kabila-wallet.png',
+        iconSize: { width: 22, height: 22 },
+        connector: KabilaConnector,
+        mobileSupported: false,
     },
     {
-      id: 'walletconnect',
-      name: 'WalletConnect',
-      icon: '/walletconnect-logo.png',
-      iconSize: { width: 32, height: 32 },
-      connector: HWCConnector,
-      mobileSupported: true
-    }
-  ];
+        id: 'walletconnect',
+        name: 'WalletConnect',
+        icon: '/walletconnect-logo.png',
+        iconSize: { width: 32, height: 32 },
+        connector: HWCConnector,
+        mobileSupported: true,
+    },
+]
 
 // Token IDs for Hedera Testnet
 export const TOKEN_IDS = {
-  hUSD: "0.0.6624430",
-  USDC: "0.0.429274"
-} as const;
+    hUSD: '0.0.6624255',
+    USDC: '0.0.429274',
+} as const
 
-export const TESTNET_MIRROR_NODE_ENDPOINT = "https://testnet.hedera.validationcloud.io/v1";
+export const TESTNET_MIRROR_NODE_ENDPOINT =
+    'https://testnet.hedera.validationcloud.io/v1'
